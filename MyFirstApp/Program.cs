9ccfdfc990cfce7ad75e219fe861b0122beb72e6
Annika Kangur <annika_kangur@hotmail.com>
2022-09-06 21:16:59 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// THIS IS A COMMENT TO MY CODE
+
+//rakendus küsib kasutajalt sisestada oma nimi
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+
+//string=sõne 
+String userName = Console.ReadLine(); //Input
+
+//Test..
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+// string interpolation - $ märk kasutab juba varasemalt defineeritud näitajat
+Console.WriteLine($"Hello, {userName} !"); //Output
+
